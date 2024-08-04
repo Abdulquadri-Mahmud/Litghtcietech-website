97df5ddedaa008eb242ec516e74842fa98682f6f
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-08-04 14:28:30 +0100

    Contact page created successfully

--- src/Components/HomeComponents/Footer.jsx
+++ src/Components/HomeComponents/Footer.jsx
@@ -1,15 +1,27 @@
 import { Box, Button, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { IoIosSend } from "react-icons/io";
 
 export default function Footer() {
+  const [send, setSend] = useState({});
+
+  const myEmail = 'adeyemiabdulquadri30@gmail.com';
+
+  const handleChange = (e) => {
+    setSend({
+      ...send,
+      [e.target.id] : e.target.value
+    });
+  }
+
   return (
     <Box bg={'gray.800'} p={{md: 10, base: 4}} >
-      <Box maxW={{'2xl': '80%', md: '90%', base: '100%'}} mx={'auto'}>
+      <Box maxW={{'2xl': '80%', md: '95%', base: '100%'}} mx={'auto'}>
         {/* <Box bg={'white'} mx={'auto'} width={{md: '270px', base: '100%'}} p={1} rounded={3}>
           <Image src='/logo.png' maxW={'200px'}/>
         </Box> */}
-        <Box mt={10} color={'white'} className='grid grid-cols-1 gap-7 md:grid-cols-3 md:place-content-center'>
+        <Box mt={10} color={'gray.300'} fontSize={14} className='grid grid-cols-1 gap-7 xl:grid-cols-4 md:grid-cols-2 md:place-content-center'>
           <Box>
             <Flex alignItems={'center'} justifyContent={{md:'start', base: 'start'}} gap={1} position={'relative'} mb={4}>
               <Box width={'12px'} height={'12px'} bg={'red.50'} rounded={'50%'}></Box>
@@ -17,7 +29,7 @@ export default function Footer() {
             </Flex>
             <Stack textAlign={{md: 'start', base: 'start'}}>
               <Text>We are earthing and lighting hazard consultants, with appreciable experience in the design, procurement, supply, installation, inspection, and maintenance of effective and up-to-date system for checking earthing deficiency and lightning strikes.</Text>
-              <Link className='text-red-500'>Read More...</Link>
+              <Link className='text-red-500' to={'/about'}>Read More...</Link>
             </Stack>
           </Box>
           <Box>
@@ -44,6 +56,17 @@ export default function Footer() {
               <Link className='text-md ' to={'/service'}>Services</Link>
               <Link className='text-md ' to={'/contact'}>Contact Us</Link>
             </Stack>
+          </Box>
+          <Box>
+            <Flex alignItems={'center'} justifyContent={{md:'start', base: 'start'}} gap={1} position={'relative'} mb={4}>
+              <Box width={'12px'} height={'12px'} bg={'red.50'} rounded={'50%'}></Box>
+              <Heading fontWeight={500} fontSize={{md: 25, base: 20}} textAlign={{md: 'start', base: 'start'}}>Leave A Message</Heading>
+            </Flex>
+            <Flex>
+              <input onChange={handleChange} type="text" id='message' placeholder='Leave a amessage' className='p-3 w-[75%] outline-none border-none rounded-none text-black'/>
+              <Button height={'49px'} px={4} rounded={'0'} bg={'red.500'} _hover={{bg: 'red.400'}} color={'white'}><Link to={`mailto:${myEmail}?subject=You can type your name here.&body=${send.message}`}><IoIosSend className='text-3xl'/></Link></Button>
+            </Flex>
+            <Text color={'gray.400'} py={2}>(We will Recieve Your Message)</Text>
           </Box>
         </Box>
         <Box pt={8} color={'white'}>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,13 +1,65 @@
-import { Box, Button, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
-import { FaFacebookF, FaLinkedinIn, FaPhone, FaTwitter } from 'react-icons/fa'
-import { FaLocationPin } from 'react-icons/fa6'
-import { RiInstagramLine } from 'react-icons/ri'
+import { Box, Button, Flex, Heading, Text, useDisclosure } from '@chakra-ui/react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { FaUser } from "react-icons/fa";
-import { MdOutlineEmail } from "react-icons/md";
+
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogContent,
+    AlertDialogOverlay,
+} from '@chakra-ui/react';
 
 export default function Contact() {
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(false);
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const cancelRef = useRef()
+
+    const getFirstName = useRef();
+    const getLastName = useRef();
+    const getMessage = useRef();
+
+    const myEmail = 'adeyemiabdulquadri30@gmail.com';
+   
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id] : e.target.value
+        });
+    }
+
+    
+    const hanldeSubmit = (e) => {
+        e.preventDefault();
+
+        const checkFirstname = getFirstName.current.value === '';
+        const checkLastname = getLastName.current.value === '';
+        const checkMessage = getMessage.current.value === '';
+
+        if (checkFirstname) {
+            setError('Firstname input field is required!');
+            return;
+        };
+        if (checkLastname) {
+            setError('Lastname input field is required!');
+            return;
+        };
+        if (checkMessage) {
+            setError('Message input field is required!');
+            return;
+        };
+
+        const sendMessage = () => {
+            setError(false);
+            const addName = `${formData.firstname} ${formData.lastname}`;
+            // const email = formData.email;
+            const message = formData.message;
+            window.location.href = `mailto:${myEmail}?subject=${addName}&body=${message}`;
+        }
+        sendMessage();
+    }
   return (
     <Box>
         <Flex justifyContent={'center'} alignItems={'center'} backgroundPosition={{md: 'bottom', base: "center"}} backgroundRepeat="no-repeat" backgroundSize="cover" backgroundImage={'/bg1.jpg'} h={'45vh'} w={'full'} bgBlendMode="multiply" className='deep'>
@@ -15,49 +67,62 @@ export default function Contact() {
             <Heading fontWeight={500}>Let's <span className="text-red-500">Get</span> In <span className="text-red-500">Touch</span> Now</Heading>
                 <Box mt={3} textAlign={'center'}>
                     <Link to={'/'}>Home / </Link>
-                    <Link to={'/blog'}>About Us</Link>
+                    <Link to={'/blog'}>Contact</Link>
                 </Box>
             </Box>
         </Flex>
-        <Flex  my={'10vh'} maxW={{md: '80%', base: '97%'}} bg={'gray.800'} color={'white'} shadow={'md'} rounded={10} flexWrap={'wrap'} mx={'auto'}>
+        <Flex  my={{xl: '10vh', md: '5vh'}} maxW={{'2xl': '80%',xl: '85%',md: '97%'}} bg={'gray.800'} color={'white'} shadow={'md'} rounded={10} flexWrap={'wrap'} mx={'auto'}>
             <Box width={{md:'45%', base: '100%'}} height={{md: '500px', base: '300px'}} pt={5} px={{md: 5, base: 5}}>
                 <iframe width={'100%'} style={{height: '100%', borderRadius: '10px'}} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.467927614716!2d3.524071474081166!3d6.588611922405158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bee4740bd527b%3A0xb19235da2044f4d9!2s1%20Akin%20Ogunlewe%20Rd%2C%20Ikorodu%2C%20104102%2C%20Lagos!5e0!3m2!1sen!2sng!4v1721227419085!5m2!1sen!2sng" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </Box>
-            <Box width={{md: '55%', base: '100%'}} p={{md: 8, base: 4}} mt={{md: 0, base: 5}}>
+            <Box width={{md: '55%', base: '100%'}} p={{xl: 8,md: 4, base: 4}} mt={{md: 0, base: 5}}>
                 <Box>
                     <Heading fontWeight={500}>Let's <span className="text-red-500">Get</span> In <span className="text-red-500">Touch</span> Now</Heading>
                     <Text color={'gray.500'} mt={4}>(We will Recieve Your Message)</Text>
                 </Box>
-                <Box mt={6}>
-                    <form>
-                        <Flex flexWrap={'wrap'} justifyContent={'center'} gap={{md: 2, base: 5}}>
-                            <Box width={{md:'48%', base: '100%'}} position={'relative'}>
-                                <label htmlFor="firstname">Firstname</label>
-                                <input type="text" id='firstname' className='py-3 pl-7 w-full rounded-md text-black outline-none border border-gray-200' placeholder='Firstname'/>
-                                <Box position={'absolute'} left={2} top={10}>
-                                    <FaUser className='text-red-500'/>
-                                </Box>
-                            </Box>
-                            <Box width={{md:'48%', base: '100%'}} position={'relative'}>
-                                <label htmlFor="firstname">Lastname</label>
-                                <input type="text" id='firstname' className='py-3 pl-7 w-full rounded-md text-black outline-none border border-gray-200' placeholder='Lastname'/>
-                                <Box position={'absolute'} left={2} top={10}>
-                                    <FaUser className='text-red-500'/>
-                                </Box>
-                            </Box>
-                        </Flex>
-                        <Box width={'100%'} mt={5} position={'relative'}>
-                            <label htmlFor="firstname">Your Address</label>
-                            <input type="text" id='firstname' className='py-3 pl-7 w-full rounded-md text-black outline-none border border-gray-200' placeholder='Example@gmail.com'/>
+                <Box mt={3}>
+                    <form onSubmit={hanldeSubmit}>
+                        <Box width={'100%'} position={'relative'}>
+                            <label htmlFor="lastname">Lastname</label>
+                            <input ref={getLastName} onChange={handleChange} type="text" id='lastname' className='py-3 pl-7 w-full rounded-md text-black outline-none border border-gray-200' placeholder='Lastname'/>
                             <Box position={'absolute'} left={2} top={10}>
-                                <MdOutlineEmail className='text-red-500'/>
+                                <FaUser className='text-red-500'/>
                             </Box>
                         </Box>
+                        <Box mt={5} width={'100%'} position={'relative'}>
+                                <label htmlFor="firstname">Firstname</label>
+                                <input ref={getFirstName} onChange={handleChange} type="text" id='firstname'  className='py-3 pl-7 w-full rounded-md text-black outline-none border border-gray-200' placeholder='Firstname'/>
+                                <Box position={'absolute'} left={2} top={10}>
+                                    <FaUser className='text-red-500'/>
+                                </Box>
+                            </Box>
                         <Box mt={5} height={'150px'} width={'100%'}>
-                            <textarea name="" style={{height: '100%', width: '100%'}} id="body" className='py-3 pl-7 w-full rounded-md text-black outline-none border border-gray-200' placeholder='Type your message here...'></textarea>
+                            <textarea ref={getMessage} onChange={handleChange} style={{height: '100%', width: '100%'}} id="message" className='py-3 pl-7 w-full rounded-md text-black outline-none border border-gray-200' placeholder='Type your message here...'></textarea>
                         </Box>
+                        {
+                            error ? (
+                                <>
+                                    <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+                                    <AlertDialogOverlay>
+                                        <AlertDialogContent>
+                                        <AlertDialogBody fontWeight={500} fontSize={18} mt={5} color={'red.500'}>
+                                            {error && error}
+                                        </AlertDialogBody>
+                                        <AlertDialogFooter>
+                                            <Button ref={cancelRef} onClick={onClose} bg={'gray.800'} color={'white'} _hover={{bg: 'gray.700'}}>
+                                                Close
+                                            </Button>
+                                        </AlertDialogFooter>
+                                        </AlertDialogContent>
+                                    </AlertDialogOverlay>
+                                    </AlertDialog>
+                                </>
+                            ) : ''
+                        }
                         <Flex justifyContent={'center'} mt={5}>
-                            <Button type='submit' rounded={'full'} height={'45px'} bg={'red.500'} _hover={{bg: 'red.600'}} color={'white'}>Send Message</Button>
+                            <Button onClick={onOpen} type='submit' rounded={'full'} height={'45px'} bg={'red.500'} _hover={{bg: 'red.600'}} color={'white'}>
+                                Send Message
+                            </Button>
                         </Flex>
                     </form>
                 </Box>
